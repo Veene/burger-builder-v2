dcb7593878ae9852954a8e53ba5d26a76ecaabea
John Pawlak <johnpawlak@gmail.com>
2018-10-28 00:18:42 -0400

    added redux functionality to side project

---
+++ playground/redux--01-start/src/store/reducer.js
@@ -0,0 +1,50 @@
+//DEPRECATED CUZ WE SPLIT IT INTO TWO
+import * as actionTypes from './actions.js'
+
+const initialState = {
+    counter: 0,
+    results: [],
+}
+
+const reducer = (state=initialState, action) => {
+    switch (action.type){
+        case actionTypes.INCREMENT :
+            return {
+                ...state,
+                counter: state.counter + 1,
+            }
+        case actionTypes.DECREMENT:
+            return {
+                ...state,
+                counter: state.counter - 1,
+            }
+        case actionTypes.ADD:
+            return {
+                ...state,
+                counter: state.counter + action.val,
+            }
+        case actionTypes.SUBTRACT:
+            return {
+                ...state,
+                counter: state.counter - action.val,
+            }
+        case actionTypes.STORE_RESULT:
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter})
+            }
+        case actionTypes.DELETE_RESULT:
+            // const id = 2;
+            // const newArray = [...state.results]
+            // newArray.splice(id, 1)
+            const updatedArray = state.results.filter((result) => result.id !== action.resultElemId)
+            return {
+                ...state,
+                results: updatedArray
+            }
+        default:
+            return state;
+    }
+}   
+
+export default reducer;
